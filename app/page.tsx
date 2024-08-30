577a5550c65974334ed7f69f61e258c78003442a
sarvagya.prateek <sarvagya.prateek@jeevansathi.com>
2024-08-30 18:25:33 +0530

    Bento Grid and images integrated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { FaHome } from "react-icons/fa";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Image from "next/image";
 
@@ -5,7 +8,11 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 justify-center items-center overflow-hidden mx-auto sm:px-10 flex flex-col">
       <div>
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );

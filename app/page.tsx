a9efc6c8a971d2ab6d0c4e020b4176b247ba8587
sarvagya.prateek <sarvagya.prateek@jeevansathi.com>
2024-09-02 17:34:03 +0530

    Projects section made

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 justify-center items-center overflow-hidden mx-auto sm:px-10 flex flex-col">
       <div>
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );

beb32ce384d7ca593162b9a3574728c0a99eabc6
aniket.chawla <aniket.chawla@jeevansathi.com>
2025-06-07 14:39:39 +0530

    Using dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
 "use client";
 
-import Experience from "@/components/Experience";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
+// import Experience from "@/components/Experience";
+// import { FloatingNav } from "@/components/ui/FloatingNav";
+// import Footer from "@/components/Footer";
+// import Grid from "@/components/Grid";
+// import Hero from "@/components/Hero";
 import Image from "next/image";
-import RecentProjects from "@/components/RecentProjects";
+// import RecentProjects from "@/components/RecentProjects";
+
+import dynamic from "next/dynamic";
 import { navItems } from "@/data";
 
+const FloatingNav = dynamic(
+  () => import("@/components/ui/FloatingNav").then((mod) => mod.FloatingNav),
+  { ssr: false },
+);
+const Hero = dynamic(() => import("@/components/Hero"), {
+  ssr: false,
+});
+const Grid = dynamic(() => import("@/components/Grid"), {
+  ssr: false,
+});
+const RecentProjects = dynamic(() => import("@/components/RecentProjects"), {
+  ssr: false,
+});
+const Experience = dynamic(() => import("@/components/Experience"), {
+  ssr: false,
+});
+const Footer = dynamic(() => import("@/components/Footer"), {
+  ssr: false,
+});
 export default function Home() {
   return (
     <main className="relative bg-black-100 justify-center items-center mx-auto sm:px-10 flex flex-col overflow-clip custom-scroll">
